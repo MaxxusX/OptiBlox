bb5c28475b3e5baafa2c42dd03fb9a92453ecd45
MaxxusX <CarterWinden@gmail.com>
2023-09-13 22:27:48 -0500

    push

---
+++ push.bat
@@ -0,0 +1,12 @@
+@echo off
+git add .
+set /p commitmsg="commit msg: "
+echo git add .
+git add .
+echo git commit -m "%commitmsg%"
+git commit -m "%commitmsg%"
+echo git push
+git push
+echo git pull
+git pull
+pause
